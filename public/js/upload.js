76f22d31a5bcfd6d4ce7c7da2e54529ad889a101
root <root@ip-172-31-42-215.us-west-2.compute.internal>
2017-04-02 18:47:12 +0000

    2017 04 03 first fix speed 2

--- public/js/upload.js
+++ public/js/upload.js
@@ -52,77 +52,77 @@ $('body').click(function(){
   $('#emailsuggest').html('');
 });
 
-/*Facebook API*/
-function statusChangeCallback(response) {
-    console.log(response);
-    // The response object is returned with a status field that lets the
-    // app know the current login status of the person.
-    // Full docs on the response object can be found in the documentation
-    // for FB.getLoginStatus().
-    if (response.status === 'connected') {
-      // Logged into your app and Facebook.
-      var fbFriend =[];
-      FB.api( //친구리스트 불러오기
-        "/me/taggable_friends?limit=2000",
-        function (response) {
-          if (response && !response.error) {
-            /* handle the result */
-            console.log(response)
-            var data = response.data;
-            var edge = response.edge;
-            for(var i=0; i<response.data.length; i++){
-              let fbFriendName= data[i].name;
-              let fbFriendPicture= data[i].picture.data.url;
+// /*Facebook API*/
+// function statusChangeCallback(response) {
+//     console.log(response);
+//     // The response object is returned with a status field that lets the
+//     // app know the current login status of the person.
+//     // Full docs on the response object can be found in the documentation
+//     // for FB.getLoginStatus().
+//     if (response.status === 'connected') {
+//       // Logged into your app and Facebook.
+//       var fbFriend =[];
+//       FB.api( //친구리스트 불러오기
+//         "/me/taggable_friends?limit=2000",
+//         function (response) {
+//           if (response && !response.error) {
+//             /* handle the result */
+//             console.log(response)
+//             var data = response.data;
+//             var edge = response.edge;
+//             for(var i=0; i<response.data.length; i++){
+//               let fbFriendName= data[i].name;
+//               let fbFriendPicture= data[i].picture.data.url;
 
-              console.log("checking friend's list");
+//               console.log("checking friend's list");
 
-              if ((fbFriendName).toLowerCase().includes(($('#email').val()).toLowerCase())){
-                Sentence = '<li class = "suggest" id = "suggestListFb'+i+'"'+' style="cursor:pointer;"> <img src="'+fbFriendPicture+'" height="20px" width="20px"> name : '+fbFriendName+'</li>';
-                var sen = '#suggestListFb'+i;
-                $('#emailsuggest').append(Sentence);
-                console.log("clicked suggest 1 "+sen);
+//               if ((fbFriendName).toLowerCase().includes(($('#email').val()).toLowerCase())){
+//                 Sentence = '<li class = "suggest" id = "suggestListFb'+i+'"'+' style="cursor:pointer;"> <img src="'+fbFriendPicture+'" height="20px" width="20px"> name : '+fbFriendName+'</li>';
+//                 var sen = '#suggestListFb'+i;
+//                 $('#emailsuggest').append(Sentence);
+//                 console.log("clicked suggest 1 "+sen);
 
-                $(sen).bind("click",function(){
-                  console.log("Clikcing Friend's Name");
-                  console.log("i: "+i+" Name: "+fbFriendName + "idValue: "+sen);
-                  $('#email').val(fbFriendName);
-                });
-              }
-            }
-          }
-        });
-    }else {
-      // The person is not logged into your app or we are unable to tell.
-      // document.getElementById('status').innerHTML = 'Please log ' +
-      // 'into this app.';
-      console.log('please log into this app');
-    }
-  }
-  function checkLoginState() {
-    FB.getLoginStatus(function(response) {
-      statusChangeCallback(response);
-    });
-  }
+//                 $(sen).bind("click",function(){
+//                   console.log("Clikcing Friend's Name");
+//                   console.log("i: "+i+" Name: "+fbFriendName + "idValue: "+sen);
+//                   $('#email').val(fbFriendName);
+//                 });
+//               }
+//             }
+//           }
+//         });
+//     }else {
+//       // The person is not logged into your app or we are unable to tell.
+//       // document.getElementById('status').innerHTML = 'Please log ' +
+//       // 'into this app.';
+//       console.log('please log into this app');
+//     }
+//   }
+//   function checkLoginState() {
+//     FB.getLoginStatus(function(response) {
+//       statusChangeCallback(response);
+//     });
+//   }
 
-  window.fbAsyncInit = function() {
-    FB.init({
-      appId      : '278220249266484',
-      cookie     : true,
-      xfbml      : true,
-      version    : 'v2.8'
-    });
-    FB.AppEvents.logPageView();
-    // checkLoginState();
-  };
+//   window.fbAsyncInit = function() {
+//     FB.init({
+//       appId      : '278220249266484',
+//       cookie     : true,
+//       xfbml      : true,
+//       version    : 'v2.8'
+//     });
+//     FB.AppEvents.logPageView();
+//     // checkLoginState();
+//   };
 
-  (function(d, s, id){
-   var js, fjs = d.getElementsByTagName(s)[0];
-   if (d.getElementById(id)) {return;}
-   js = d.createElement(s); js.id = id;
-   js.src = "//connect.facebook.net/en_US/sdk.js";
-   fjs.parentNode.insertBefore(js, fjs);
- }(document, 'script', 'facebook-jssdk'));
- /*End of Facebook API*/ 
+//   (function(d, s, id){
+//    var js, fjs = d.getElementsByTagName(s)[0];
+//    if (d.getElementById(id)) {return;}
+//    js = d.createElement(s); js.id = id;
+//    js.src = "//connect.facebook.net/en_US/sdk.js";
+//    fjs.parentNode.insertBefore(js, fjs);
+//  }(document, 'script', 'facebook-jssdk'));
+//  /*End of Facebook API*/ 
 
 //upload autocomplete
 $( "#email" ).autocomplete({
@@ -138,7 +138,7 @@ $( "#email" ).autocomplete({
       success: function( data ) {
         response( data );
         console.log('data response success');
-        checkLoginState();
+        // checkLoginState();
       },error: function(){
         console.log("AJAX Search error");
       }
